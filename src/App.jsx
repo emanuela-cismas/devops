5a56d752590032a2f53d88ed4286f293a4df3c1c
emanuela-cismas <73247584+emanuela-cismas@users.noreply.github.com>
2021-11-21 20:55:11 +0200

    Ignore this shit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import { Products } from "./pages/Products";
 import { Register } from "./pages/Register";
 const App = () => {
-  return <Register />;
+  return <Home />;
 };
 
 export default App;
